d11573a1172126f362263cd273df0a254a8bea1a
Noah <noah.singer@gmail.com>
2011-05-26 13:49:16 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Distance::Application.config.secret_token = 'f830e0e4962335cbfdf8be0ae85161041e26aa060cc191b74c236941ca154758911da3539c50da76b73c0d44f8436bda7f34c6fb4d8c009780d27883a6a3c084'
